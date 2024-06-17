50deaed721568de8b072eff43eab39ca4ca9ece6
kartik294 <kartikgeu@gmail.com>
2024-06-17 11:16:15 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,45 @@
-import logo from './logo.svg';
-import './App.css';
-
+import { useState, useEffect } from "react";
+import "./App.css";
 function App() {
+  const [time, setTime] = useState(0);
+  const [running, setRunning] = useState(0);
+
+  useEffect(() => {
+    let timer;
+    if (running) {
+      timer = setInterval(() => {
+        setTime((prevTime) => prevTime + 1);
+      });
+    } else if (!running) {
+      clearInterval(timer);
+    }
+
+    return () => clearInterval(timer);
+  }, [running]);
+
+  const handleStartStop = () => {
+    setRunning(!running);
+  };
+
+  const handleReset = () => {
+    setTime(0);
+    setRunning(false);
+  };
+
+  const formatTime = () => {
+    const minutes = Math.floor(time / 60);
+    const seconds = Math.floor(time % 60);
+    return `${minutes}:${seconds < 10 ? `0${seconds}` : seconds}`;
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <h1>Stop Watch</h1>
+      <div>
+        <span>Time :{formatTime(time)}</span>
+      </div>
+
+      <button onClick={handleStartStop}>{running ? "Stop" : "Start"}</button>
+      <button onClick={handleReset}>Reset</button>
     </div>
   );
 }
